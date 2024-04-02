49fb2c7cba5fee378e75dec1fe7be953675283e8
Diego Dmitry <diegodmitry@hotmail.com>
2024-04-02 16:00:45 +0100

    Removed unnecessarily line.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.getElementById('shareImage').onclick = function() {
   // The URL to the image you want to share
   const imageUrl = "https://meeting-god-ina.vercel.app/"; 
-  // const imageUrl = "https://meeting-god-ina.vercel.app/images/encontro.jpeg"; 
   
   // Custom message you want to appear in the chat
   const customMessage = "Descobri algo transformador este fim de semana e estou ansioso(a) para compartilhar. Acesse o link e descubra!"; 
