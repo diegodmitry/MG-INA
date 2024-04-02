1a9aae80f10a658a08341c7671bbf1a12a95b6d6
Diego Dmitry <diegodmitry@hotmail.com>
2024-04-02 12:41:45 +0100

    changed path image in javascript. change relative path to fully qualified URL.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('shareImage').onclick = function() {
-  var imageUrl = "./images/encontro.jpeg"; // URL of the image to share
+  var imageUrl = "https://meeting-god-ina.vercel.app/images/encontro.jpeg"; // URL of the image to share
   var whatsappUrl = `https://wa.me/?text=Check out this image: ${encodeURIComponent(imageUrl)}`;
 
   window.open(whatsappUrl, '_blank').focus();
