bc329803183b1c49d0579f560c4aa03c39b49175
Diego Dmitry <diegodmitry@hotmail.com>
2024-04-02 15:16:12 +0100

    Change the sentence on WhatsApp.

--- script.js
+++ script.js
@@ -8,10 +8,13 @@
 document.getElementById('shareImage').onclick = function() {
   // The URL to the image you want to share
   const imageUrl = "https://meeting-god-ina.vercel.app/images/encontro.jpeg"; 
+  
   // Custom message you want to appear in the chat
-  const customMessage = "Descobri algo transformador este fim de semana e estou ansioso(a) para compartilhar. Acesse o link e descubra?"; 
+  const customMessage = "Descobri algo transformador este fim de semana e estou ansioso(a) para compartilhar. Acesse o link e descubra!"; 
+  
   // Encoding the message and the image URL to ensure it's formatted correctly for a URL
   const encodedMessage = encodeURIComponent(customMessage + " " + imageUrl);
+  
   // Creating the WhatsApp share link with your custom message
   const whatsappUrl = `https://wa.me/?text=${encodedMessage}`;
 
